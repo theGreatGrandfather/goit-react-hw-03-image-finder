f73b27adcbbee4ed850db7d98e9305be043bb267
Praded3 <pradid.store@gmail.com>
2023-08-28 23:17:23 +0300

    12

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -77,8 +77,8 @@ export class App extends Component {
     this.setState(prevState => ({
       loadingPage: prevState.loadingPage +=1
     }));
-    console.log('first');
-    scroll.scrollMore(500);
+    // console.log('first');
+    // scroll.scrollMore(500);
   }
 
   render() {
